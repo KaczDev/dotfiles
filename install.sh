0e7351050e5d7d695836afe3851f1a29134c3f2b
KaczDev <kaczdev@github.com>
2025-02-21 12:21:13 +0000

    set ruby to latest

--- install.sh
+++ install.sh
@@ -15,8 +15,13 @@ function link_files() {
 	  sudo ln -sf /workspaces/github/bin/solargraph /usr/local/bin/solargraph
 	  sudo ln -sf /workspaces/github/bin/safe-ruby /usr/local/bin/safe-ruby
 	  sudo update-locale LANG=en_US.UTF-8 LC_TYPE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+	  # Configure ruby with rbenv
+	  ruby_latest="$(rbenv install -l | grep -v - | tail -1)"
+	  rbenv install --skip-existing "$ruby_latest"
+	  rbenv global "$ruby_latest"
 	fi
 }
+
 function install_software(){
 	sudo apt install -y stow fzf
 	sudo apt remove -y ripgrep
@@ -27,20 +32,20 @@ function install_software(){
 function setup_software(){
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-    echo "TMUX plugins installed" >> ~/install.log
-    echo `date +"%Y-%m-%d %T"` >> ~/install.log;
+	echo "TMUX plugins installed" >> ~/install.log
+	date +"%Y-%m-%d %T" >> ~/install.log;
 	nvim --headless "+Lazy! sync" +qa >> ~/install.log 
 }
 
 
 echo '💽 Installing software' >> ~/install.log;
-echo `date +"%Y-%m-%d %T"` >> ~/install.log;
+date +"%Y-%m-%d %T" >> ~/install.log;
 install_software
 echo '🔗 Linking files.' >> ~/install.log;
-echo `date +"%Y-%m-%d %T"` >> ~/install.log;
+date +"%Y-%m-%d %T" >> ~/install.log;
 link_files
 echo '👩<200d>🔧 configure software' >> ~/install.log;
-echo `date +"%Y-%m-%d %T"` >> ~/install.log;
+date +"%Y-%m-%d %T" >> ~/install.log;
 setup_software
 echo '✅ Done!' >> ~/install.log;
-echo `date +"%Y-%m-%d %T"` >> ~/install.log;
+date +"%Y-%m-%d %T" >> ~/install.log;
